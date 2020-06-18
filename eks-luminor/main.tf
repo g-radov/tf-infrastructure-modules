64f25e70cc1317c825d71d85ece43f506e861045
Georgijs Radovs <g_radov@mailfence.com>
2020-06-18 21:25:54 +0300

    Update module references

--- eks-luminor/main.tf
+++ eks-luminor/main.tf
@@ -10,7 +10,7 @@ locals {
 module "vpc" {
   source               = "terraform-aws-modules/vpc/aws"
   version              = "2.39.0"
-  name                 = "vpc-${var.name}"
+  name                 = var.name
   cidr                 = var.vpc_cidr_block
   azs                  = var.azs
   private_subnets      = var.private_subnets
@@ -40,13 +40,13 @@ provider "kubernetes" {
   version                = "~> 1.9"
 }
 
-module "this-cluster" {
+module "cluster" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "12.1.0"
   cluster_name    = var.name
   cluster_version = var.cluster_version
-  subnets         = module.this-vpc.private_subnets
-  vpc_id          = module.this-vpc.vpc_id
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
   worker_groups = [
     {
       instance_type = var.wg_instance_type
@@ -63,7 +63,7 @@ module "iam-role-admin" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
   version = "~> 2.0"
   trusted_role_arns = [
-    module.this-iam-user-admin.this_iam_user_arn
+    module.iam-user-admin.this_iam_user_arn
   ]
   create_role       = true
   role_name         = "eks-luminor-admin"
@@ -96,7 +96,7 @@ module "iam-role-read-only" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
   version = "~> 2.0"
   trusted_role_arns = [
-    module.this-iam-user-read-only.this_iam_user_arn
+    module.iam-user-read-only.this_iam_user_arn
   ]
   create_role       = true
   role_name         = "eks-luminor-read-only"
