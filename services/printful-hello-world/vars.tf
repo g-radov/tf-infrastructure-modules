b43685db647e12c2bed30c2be49930c8ed93a710
gradov <g_radov@mailfence.com>
2020-09-08 23:58:05 +0300

    Update `var.cpu.default` Update `var.memory.default`

--- services/printful-hello-world/vars.tf
+++ services/printful-hello-world/vars.tf
@@ -1,18 +1,18 @@
 variable "family" {
   type        = string
-  description = "A unique name for your task definition"
+  description = "A unique name for your task definition."
 }
 
 variable "cpu" {
   type        = number
   description = "The number of cpu units used by the task."
-  default     = 10
+  default     = 512
 }
 
 variable "memory" {
   type        = number
   description = "The number of cpu units used by the task."
-  default     = 128
+  default     = 1024
 }
 
 variable "name" {
@@ -47,6 +47,24 @@ variable "container_name" {
 variable "container_port" {
   type        = number
   description = "The port on the container to associate with the load balancer."
+  default     = null
+}
+
+variable "vpc_id" {
+  description = "VPC ID for ECS service security group."
+  type        = string
+  default     = null
+}
+
+variable "subnets" {
+  type        = list(string)
+  description = "A list of subnets to launch ECS service in."
+  default     = null
+}
+
+variable "security_groups" {
+  type        = list(string)
+  description = "A list of security groups to assign to ECS service"
   default     = null
 }
 
