d611a00abd1ec65f37fcea273980e66b34fe5201
gradov <g_radov@mailfence.com>
2020-09-08 23:54:21 +0300

    Update `target_groups.packend_port` Update `target_groups.target_type` Add `target_groups.health_check.matcher` Update `this_alb_sg_server` rules Update `this_alb_sg_client` rules Update code formatting

--- platforms/ecs-fargate/main.tf
+++ platforms/ecs-fargate/main.tf
@@ -8,14 +8,17 @@ module "this_alb" {
   vpc_id             = var.vpc_id
   subnets            = var.subnets
   security_groups = [
-    module.this_alb_sg.this_security_group_id
+    module.this_alb_sg_server.this_security_group_id
   ]
   target_groups = [
     {
       name             = var.name
       backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
+      backend_port     = 8080
+      target_type      = "ip"
+      health_check = {
+        matcher = "200,403"
+      }
     }
   ]
   http_tcp_listeners = [
@@ -25,23 +28,49 @@ module "this_alb" {
       target_group_index = 0
     }
   ]
-  tags = var.tags
+  tags              = var.tags
+  target_group_tags = var.tags
 }
 
-module "this_alb_sg" {
+module "this_alb_sg_server" {
   source              = "terraform-aws-modules/security-group/aws"
-  name                = var.name
-  description         = "${var.name} - Security Group"
+  name                = "${var.name}-alb-server"
+  description         = "${var.name} - ALB security group (server)"
   vpc_id              = var.vpc_id
   ingress_cidr_blocks = var.ingress_cidr_blocks
   ingress_rules = [
     "http-80-tcp"
   ]
+  ingress_with_source_security_group_id = [
+    {
+      rule                     = "all-all"
+      source_security_group_id = module.this_alb_sg_client.this_security_group_id
+    }
+  ]
+  egress_with_cidr_blocks = [
+    {
+      rule        = "all-all"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
+
+module "this_alb_sg_client" {
+  source      = "terraform-aws-modules/security-group/aws"
+  name        = "${var.name}-alb-client"
+  description = "${var.name} - security group (client)"
+  vpc_id      = var.vpc_id
+  ingress_with_source_security_group_id = [
+    {
+      rule                     = "all-all"
+      source_security_group_id = module.this_alb_sg_server.this_security_group_id
+    }
+  ]
 }
 # ALB configuration end
 # =====================
 
-# ECS Cluster configuration start
+# ECS cluster configuration start
 # ===============================
 resource "aws_ecs_cluster" "this" {
   name = var.name
@@ -50,4 +79,5 @@ resource "aws_ecs_cluster" "this" {
     "FARGATE_SPOT"
   ]
 }
-
+# ECS cluster configuration end
+# =============================
