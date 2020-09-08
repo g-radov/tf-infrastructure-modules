76af7304b9af0148d77ba458f2f2b8eb372127cb
gradov <g_radov@mailfence.com>
2020-09-08 23:55:57 +0300

    Add `execution_role_arn` Add `aws_ecs_service.this.network_configuration` Add `module.this_ecs_sg` Add `data.aws_iam_policy_document.this`

--- services/printful-hello-world/main.tf
+++ services/printful-hello-world/main.tf
@@ -4,13 +4,15 @@ resource "aws_ecs_task_definition" "this" {
   requires_compatibilities = [
     "FARGATE"
   ]
-  cpu           = var.cpu
-  memory        = var.memory
-  task_role_arn = module.this_iam_role.this_iam_role_arn
+  cpu                = var.cpu
+  memory             = var.memory
+  task_role_arn      = module.this_iam_role.this_iam_role_arn
+  execution_role_arn = module.this_iam_role.this_iam_role_arn
+  network_mode       = "awsvpc"
   volume {
-    name      = "service-storage"
-    host_path = "/ecs/service-storage"
+    name = "service-storage"
   }
+  tags = var.tags
 }
 
 resource "aws_ecs_service" "this" {
@@ -19,20 +21,54 @@ resource "aws_ecs_service" "this" {
   launch_type     = "FARGATE"
   task_definition = aws_ecs_task_definition.this.id
   desired_count   = var.desired_count
-  iam_role        = module.this_iam_role.this_iam_role_arn
-  depends_on = [
-    module.this_iam_role.this_policy_arn
-  ]
-  ordered_placement_strategy {
-    type  = "binpack"
-    field = "cpu"
-  }
   load_balancer {
     target_group_arn = var.target_group_arn
     container_name   = var.container_name
     container_port   = var.container_port
   }
-  tags = var.tags
+  network_configuration {
+    subnets = var.subnets
+    security_groups = flatten(
+      [
+        module.this_ecs_sg.this_security_group_id,
+        var.security_groups,
+      ]
+    )
+  }
+  # tags = var.tags
+}
+
+module "this_ecs_sg" {
+  source      = "terraform-aws-modules/security-group/aws"
+  name        = var.name
+  description = "${var.name} - Security Group"
+  vpc_id      = var.vpc_id
+  ingress_with_self = [
+    {
+      rule = "all-all"
+    },
+  ]
+  egress_with_cidr_blocks = [
+    {
+      rule        = "all-all"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    sid = "AllowECSCreateCloudWatchLogs"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams"
+    ]
+    resources = [
+      "arn:aws:logs:*:*:*"
+    ]
+  }
 }
 
 module "this_iam_policy" {
@@ -41,20 +77,7 @@ module "this_iam_policy" {
   name        = var.name
   path        = "/"
   description = "${var.cluster} - IAM role policy"
-  policy      = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+  policy      = data.aws_iam_policy_document.this.json
 }
 
 module "this_iam_role" {
@@ -63,7 +86,12 @@ module "this_iam_role" {
   create_role       = true
   role_requires_mfa = false
   role_name         = var.name
+  role_description  = "${var.name} - IAM role"
+  trusted_role_services = [
+    "ecs.amazonaws.com",
+    "ecs-tasks.amazonaws.com"
+  ]
   custom_role_policy_arns = [
-    module.this_iam_policy.arn,
+    module.this_iam_policy.arn
   ]
 }
